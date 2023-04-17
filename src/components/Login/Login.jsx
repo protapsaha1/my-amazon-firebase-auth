8627540baae1eb69be6da8f7a5120a5d395a6602
protapsaha1 <webprotap09@gmail.com>
2023-04-18 01:44:32 +0600

    create sign in and sign up form and add firebase

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,10 +1,40 @@
 import React from 'react';
 import './Login.css';
+import { Link } from 'react-router-dom';
+// import { FontAwesomeIcon , } from '@fortawesome/react-fontawesome';
 
 const Login = () => {
+
+    const handleLogin = event => {
+        event.preventDefault();
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(email,password)
+    }
     return (
-        <div>
-            <h2>Login coming sooon</h2>
+        <div className='form-container'>
+            <h2 className='form-title'>Login</h2>
+            <form onSubmit={handleLogin}>
+                <div className="form-control">
+                    <label htmlFor="">Email</label>
+                    <br />
+                    <input type="email" name="email" id="email" required />
+                </div>
+                <div className="form-control">
+                    <label htmlFor="">Password</label>
+                    <br />
+                    <input type="password" name="password" id="password" required />
+                </div>
+                <div>
+                    <input type="checkbox" name="checkbox" id="checkbox" className='pass-visible' />
+                    <label htmlFor="">See password</label>
+                </div>
+                <button className='login-btn'>Login</button>
+                <p className='create-regis'><small>New to amazon? <Link to="/sign-up" className='link'>Create New Account</Link></small></p>
+            </form>
+            <p className='divider'>____________________________ or ____________________________</p>
+            <button className='google-btn'>Continue with Google</button>
         </div>
     );
 };
--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -1,10 +1,49 @@
 import React from 'react';
 import './SignUp.css';
+import { Link } from 'react-router-dom';
 
 const SignUp = () => {
+
+    const handleSignUp = event => {
+        event.preventDefault();
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(email,password)
+    }
+
     return (
-        <div>
-            
+        <div className='signup-form-container'>
+            <h2 className='form-title'>Sign up</h2>
+            <form onSubmit={handleSignUp}>
+                <div className="form-control">
+                    <label htmlFor="">Email</label>
+                    <br />
+                    <input type="email" name="email" id="email" required />
+                </div>
+                <div className="form-control">
+                    <label htmlFor="">Password</label>
+                    <br />
+                    <input type="password" name="password" id="password" required />
+                </div>
+                <div>
+                    <input type="checkbox" name="checkbox" id="checkbox" className='pass-visible' />
+                    <label htmlFor="">See password</label>
+                </div>
+                <div className="form-control">
+                    <label htmlFor="">Confirm Password</label>
+                    <br />
+                    <input type="password" name="password" id="confirm-password" required />
+                </div>
+                <div>
+                    <input type="checkbox" name="checkbox" id="checkbox-visible" className='pass-visible' />
+                    <label htmlFor="">See password</label>
+                </div>
+                <button className='login-btn'>Sign up</button>
+                <p className='create-regis'><small>Already have an Account? <Link to="/login" className='link'>Please Login</Link></small></p>
+            </form>
+            <p className='divider'>____________________ or ____________________</p>
+            <button className='google-btn'>Continue with Google</button>
         </div>
     );
 };
